bc59460c58a78eafc2b374438982452c3d33da44
Anthony Justin Duong <anthonyjustinduong2000@gmail.com>
2022-11-22 10:21:13 -0500

    (Testing branch protection)

--- src/context/UserContext.js
+++ src/context/UserContext.js
@@ -7,6 +7,7 @@ export const UserContext = createContext({
 });
 
 export async function getUserProfile(id) {
+  // Gets the user profile from the database
   try {
     const userDocRef = firebase.firestore().collection('users').doc(id)
     const userDoc = await userDocRef.get()
